322964b2b9008a9fc5077ab63fccf6ac8c2b698a
spiethc0590 <43735559+spiethc0590@users.noreply.github.com>
2021-10-08 10:19:27 -0500

    Delete credentials copy.py

--- credentials copy.py
+++
@@ -1,6 +0,0 @@
-#Mandatory
-MERAKI_API_KEY='d65a0f983403c6c57c480617abdee047ee1d8144'
-MERAKI_BASEURL='https://api.meraki.com/api/v1'
-WEBHOOK_SERVER_NAME = "MY SERVER"
-WEBHOOK_SERVER_URL = "https://accd630679c2.ngrok.io"
-WEBHOOK_PASSWORD = "Pa$$W0rd"
